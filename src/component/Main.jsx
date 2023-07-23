d4e8d5f0c64317f52fbf5d40ed3b2747677a9a95
SamedovResul <samedovrasul7@gmail.com>
2023-07-23 16:06:09 +0400

    .

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-
 import SectionOne from "./Section_One"
+import SectionTwo from "./Section_Two"
+import SectionThree from "./Section_Three"
+import SectionFour from "./Section_Four"
 
 const Main = () => {
   return (
     <>
       <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+
     </>
   )
 }
