73dda8be752819ed1300369559100fb6a39186ae
SamedovResul <samedovrasul7@gmail.com>
2023-07-23 15:55:07 +0400

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import MainPage from './component/Section_One'
+import MainPage from './component/MainPage'
 import RequestDemo from './component/RequestDemo';
 // import Navigation from './component/navigation';
 // import Footer from './component/Footer';
